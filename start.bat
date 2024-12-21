ae7dbcf62853a0dc04d7bc1f32a94d77ccc16622
zMatheus222 <matheus.samuel00@hotmail.com>
2024-12-21 12:44:46 -0300

    try to disable QuickEdit on start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,14 @@
 @echo off
+:: Disable Quick Edit Mode
+REG ADD HKEY_CURRENT_USER\Console /v QuickEdit /t REG_DWORD /d 0 /f
+
+:: Change to the directory containing the batch file
 cd /d %~dp0
+
+:: Run the Node.js application
 node main.js
+
+:: Re-enable Quick Edit Mode (optional)
+REG ADD HKEY_CURRENT_USER\Console /v QuickEdit /t REG_DWORD /d 1 /f
+
 pause
